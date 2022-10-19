ea8dab0e49e9654a73dbd495be6371c53384a603
SDann <smdann17@gmail.com>
2022-10-18 19:06:36 -0500

    Added a file for database queries. In index.js, created initial prompts for the user to select what they would like to do and then based on their selection, the application will be directed to the appropriate function. Functions to be built.

--- index.js
+++ index.js
@@ -16,3 +16,44 @@ const db = mysql.createConnection(
   console.log(`Connected to database.`)
 );
 
+// Prompts for user
+const employeeManager = () => {
+  console.log("Welcome to the Employee Management System.");
+
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "userSelection",
+      choices: ["Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+    },
+  ]) 
+  
+  .then((response) => {
+    const userResponse = response.userSelection;
+    if (userResponse === "Add Employee") {
+      console.log("Add employee selected by user")
+      addEmployee()
+    }
+    if (userResponse === "Update Employee Role") {
+      updateRole()
+    }
+    if (userResponse === "View All Roles") {
+      viewRoles()
+    }
+    if (userResponse === "Add Role") {
+      addRole()
+    }
+    if (userResponse === "View All Departments") {
+      viewDepartments()
+    }
+    if (userResponse === "Add Department") {
+      addDepartment()
+    } 
+    if (userResponse === "Quit") {
+      quitApplication()
+    }
+  })
+};
+
+employeeManager()
